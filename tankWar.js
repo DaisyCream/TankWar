ad807d35292ae1b79038d17e872ab805daa0f9b4
DaisyCream@qq.com <DaisyCream@qq.com>
2015-09-23 18:45:33 +0800

    find the problem of transtion

--- tankWar.js
+++ tankWar.js
@@ -25,13 +25,18 @@ AIM.targetY = parseInt(cannon.style.top);
 
 /***************************The enemy's appear*****************************/
 var ENEMY = function(){};
-ENEMY.side = windowWidth*0.03 + 'px';
+ENEMY.side = parseInt(windowWidth*0.03);
+ENEMY.createTime = function(){
+    return (parseInt(Math.random()*10)+10)*1000;
+};
+
 /***
  * This function is random enemy's move speed;
  * @returns {number}
  */
-ENEMY.moveSpeed = function(){
-    return parseInt(Math.random()*6)+1;
+ENEMY.moveSpeed = function(target){
+    target.moveSpeed = parseInt(Math.random()*6)+1;
+    return target.moveSpeed;
 };
 
 
@@ -44,23 +49,31 @@ ENEMY.moveSpeed = function(){
 ENEMY.initPosition = function(target){
     var ranChooseSide = parseInt(Math.random()*4)+1;
     var ranChooseXLong = parseInt(Math.random()*(windowWidth-ENEMY.side));
-    var ranChooseYLong = parseInt(Math.random()*(windowWidth-ENEMY.side));
+    var ranChooseYLong = parseInt(Math.random()*(windowHeight-ENEMY.side));
     switch (ranChooseSide){
         case 1:
             target.style.left = ranChooseXLong + 'px';
             target.style.top = 0 + 'px';
+            console.log(target.style.left);
+            console.log(target.style.top);
             break;
         case 2:
             target.style.left = windowWidth + 'px';
-            target.style.top = ranChooseYLong + 'px';
+            target.style.top = ranChooseYLong + 'px'
+            console.log(target.style.left);
+            console.log(target.style.top);
             break;
         case 3:
             target.style.left = ranChooseXLong + 'px';
             target.style.top = windowHeight + 'px';
+            console.log(target.style.left);
+            console.log(target.style.top);
             break;
         case 4:
             target.style.left = 0 + 'px';
             target.style.top = ranChooseYLong + 'px';
+            console.log(target.style.left);
+            console.log(target.style.top);
             break;
     }
 
@@ -68,7 +81,7 @@ ENEMY.initPosition = function(target){
 
 
 function addEnemyAnimation(target){
-    target.style.webkitTransition = 'left ' + ENEMY.moveSpeed()+'s' + ',top ' + ENEMY.moveSpeed() + 's';
+    target.style.webkitTransition = 'top ' + ENEMY.moveSpeed(target) + 's' + ",left " + target.moveSpeed+'s';
     target.style.top = windowHeight/2 + 'px';
     target.style.left = windowWidth/2 + 'px';
 }
@@ -77,9 +90,22 @@ function addEnemyAnimation(target){
  * The function is create enemy
  */
 
+
 function createEnemy(){
+    var enemy = document.createElement('div');
+    enemy.setAttribute('id','sd');
+    enemy.setAttribute('class','enemy');
+    enemy.style.width = ENEMY.side + 'px';
+    enemy.style.height = ENEMY.side + 'px';
+    alert(ENEMY.side);
+    cannon.parentNode.appendChild(enemy);
+    ENEMY.initPosition(enemy);
+    addEnemyAnimation(enemy);
 
-
+    //setTimeout(createEnemy, ENEMY.createTime());
 }
 
 
+window.onload = function(){
+    createEnemy();
+};
